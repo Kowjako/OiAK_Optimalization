86a97a200abf4fd612bf41e33149064f0a3ff7fb
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-03-26 13:16:33 +0100

    Add header realization

--- bsttree.h
+++ bsttree.h
@@ -1,6 +1,21 @@
 #ifndef BSTTREE_H
 #define BSTTREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <random>
+#include <fstream>
+
+/* Struktura opisujaca pojedynczy wezel drzewa */
+struct nodeBST {
+    int value;  //klucz wezla
+    nodeBST* parentItem;    //wskaznik na rodzica
+    nodeBST* leftItem;  //wskaznik na lewego potomka
+    nodeBST* rightItem;     //wskaznik na prawego potomka
+};
+
+using namespace std;
 
 class bstTree
 {
@@ -8,9 +23,20 @@ class bstTree
         bstTree();
         virtual ~bstTree();
 
+        nodeBST* root = NULL; //korzen drzewa
+        void addValue(int value); //dodanie elementu do drzewa
+        nodeBST* addNewNode(nodeBST* parent, int value); //funkcja tworzenia nowego wierzcholku
+        void deletePtr(nodeBST* deletedNode); //usuniecie wierzcholka poprzez jego wskaznik
+        void deleteVertex(int value); //usuniecie wierzcholku poprzez jego wartosc
+        nodeBST* getMaxNode(nodeBST* parent); //funkcja do szukania poprzednika
+        nodeBST* findElement(int value); //funkcja wyszukiwania elementu w drzewie
+        void showTree(nodeBST* startNode, long n); //funkcja wypisujaca drzewo w konsoli
+        void readFromFile(string filename);  //funkcja wczytujaca drzewo z pliku.
+
     protected:
 
     private:
+        void clearTree(nodeBST* node); //funkcja usuwajaca drzewo
 };
 
 #endif // BSTTREE_H
