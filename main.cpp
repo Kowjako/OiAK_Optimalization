9599fbb513687274dd9f9d5e81f4d8d5430fad9d
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-23 12:49:59 +0200

    Add menu and time-reading realization

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-
 #include <iostream>
-
 #include <windows.h>
 #include "bsttree.h"
+
 using namespace std;
+
+long long int frequency, start, elapsed;
+bstTree tree;
 
 long long int read_QPC()
 {
@@ -12,35 +14,66 @@ long long int read_QPC()
     return((long long int)count.QuadPart);
 }
 
-long long int frequency, start, elapsed;
-
 int main()
 {
+    int operationNumber;
+    double sum = 0;
     QueryPerformanceFrequency((LARGE_INTEGER*)&frequency);
+    do {
+        cout<<"1. Wypelnij drzewo"<<endl;
+        cout<<"2. Pomiar czasu dodania"<<endl;
+        cout<<"3. Pomiar czasu usuwania"<<endl;
+        cout<<"4. Pomiar czasu wyszukiwania"<<endl;
+        cout<<"5. Rownowazenie drzewa"<<endl;
+        cout<<"6. Zakoncz program"<<endl;
+        cin>>operationNumber;
+        switch(operationNumber) {
+            case 1:
+                    int n;
+                    cout<<"Wprowadz liczbe wierzcholkow"<<endl;
+                    cin>>n;
+                    for(int i=0;i<n;i++) {
+                        tree.addValue(rand()%32000);
+                    }
+                    break;
+            case 2:
+                    sum = 0;
+                    for(int i=0;i<100;i++) {
+                        start = read_QPC();
+                        tree.addValue(rand()%32000);
+                        elapsed = read_QPC() - start;
+                        sum+=elapsed;
+                    }
+                    cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
+                    break;
+            case 3:
+                    sum = 0;
+                    for(int i=0;i<100;i++) {
+                        start = read_QPC();
+                        tree.deleteVertex(rand()%32000);
+                        elapsed = read_QPC() - start;
+                        sum+=elapsed;
+                    }
+                    cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
+                    break;
+            case 4:
+                    sum = 0;
+                    for(int i=0;i<100;i++) {
+                        start = read_QPC();
+                        tree.findElement(rand()%32000);
+                        elapsed = read_QPC() - start;
+                        sum+=elapsed;
+                    }
+                    cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
+                    break;
+            case 5:
+                    tree.algorithmDSW();
+                    break;
+            default:
+                    break;
+        }
+    }
+    while(operationNumber!=6);
 
-    bstTree tree;
-    tree.addValue(12);
-    tree.addValue(6);
-    tree.addValue(17);
-    tree.addValue(3);
-    tree.addValue(7);
-    tree.addValue(1);
-    tree.addValue(4);
-    tree.addValue(9);
-    tree.addValue(11);
-    tree.addValue(15);
-    tree.addValue(22);
-    tree.addValue(13);
-    tree.addValue(16);
-    tree.addValue(20);
-    tree.addValue(30);
-    cout<<endl;
-    tree.algorithmDSW();
-    cout<<endl;
-    tree.showTree(tree.root,0);
-    if(tree.findElement(15));
-    cout<<endl;
-    cout<<endl;
-    cout<<"Element 15 znaleziony"<<endl;
-    return 0;
+	return(0);
 }
