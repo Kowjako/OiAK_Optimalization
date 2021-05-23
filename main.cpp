637ef2457641813324ed689dc330ad7a28635c87
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-23 13:03:34 +0200

    Add rdtsc() function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <windows.h>
+#include <cstdlib>
+#include <stdint.h>
+#include <intrin.h>
 #include "bsttree.h"
-
+#ifdef _WIN32
+#endif
 using namespace std;
+
+uint64_t rdtsc(){
+    return __rdtsc();
+}
 
 long long int frequency, start, elapsed;
 bstTree tree;
@@ -18,6 +26,8 @@ int main()
 {
     int operationNumber;
     double sum = 0;
+    uint64_t tick, elapsedTick;
+    uint64_t rdtscSum = 0;
     QueryPerformanceFrequency((LARGE_INTEGER*)&frequency);
     do {
         cout<<"1. Wypelnij drzewo"<<endl;
@@ -38,36 +48,52 @@ int main()
                     break;
             case 2:
                     sum = 0;
+                    rdtscSum = 0;
                     for(int i=0;i<100;i++) {
                         start = read_QPC();
+                        tick = rdtsc();
                         tree.addValue(rand()%32000);
+                        elapsedTick = rdtsc() - tick;
                         elapsed = read_QPC() - start;
                         sum+=elapsed;
+                        rdtscSum+=elapsedTick;
                     }
+                    cout << "CFE = " << (rdtscSum) / 100 <<endl;
                     cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
                     break;
             case 3:
                     sum = 0;
+                    rdtscSum = 0;
                     for(int i=0;i<100;i++) {
                         start = read_QPC();
+                        tick = rdtsc();
                         tree.deleteVertex(rand()%32000);
+                        elapsedTick = rdtsc() - tick;
                         elapsed = read_QPC() - start;
                         sum+=elapsed;
+                        rdtscSum+=elapsedTick;
                     }
+                    cout << "CFE = " << (rdtscSum) / 100 <<endl;
                     cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
                     break;
             case 4:
                     sum = 0;
+                    rdtscSum = 0;
                     for(int i=0;i<100;i++) {
                         start = read_QPC();
+                        tick = rdtsc();
                         tree.findElement(rand()%32000);
+                        elapsedTick = rdtsc() - tick;
                         elapsed = read_QPC() - start;
                         sum+=elapsed;
+                        rdtscSum+=elapsedTick;
                     }
+                    cout << "CFE = " << (rdtscSum) / 100 <<endl;
                     cout << "Time [us] = "<< (1000000.0 * sum)/(frequency*100)<<endl;
                     break;
             case 5:
                     tree.algorithmDSW();
+                    cout<<"Drzewo zrownowazone!"<<endl;
                     break;
             default:
                     break;
